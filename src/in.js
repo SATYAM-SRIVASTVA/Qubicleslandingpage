5bbe158505f3d35c1d93b8098848040502aefda6
SATYAM-SRIVASTVA <satyam442001@gmail.com>
2022-04-08 11:11:47 +0530

    Clean

--- src/in.js
+++
@@ -1,4 +0,0 @@
-console.log("shubhajhbhjgjhgjhjhm")
-console.log("igsyfdyfgfgchxghcvhg");
-console.log("igsyfdyfgfgchxghcvhg");
-console.log("ghar se the chale toh ye baat ho gyi aashiqui aa gyi koi chij jachti nhi hai ")
